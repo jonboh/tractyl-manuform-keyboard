0106fab06f7ab4f20fbfc1bbd53ade57f28dc6fc
jonboh <jon.bosque.hernando@gmail.com>
2023-02-12 10:45:02 +0100

    fix code for static ball bearing

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -883,21 +883,17 @@
 ;; Trackball ;;
 ;;;;;;;;;;;;;;;
 
-(def dowel-depth-in-shell -0.75) ; -2.25 is just tangentially touching
-(def bearing-protrude (- 1.5 dowel-depth-in-shell)) ; Radius of the baring minus how deep it's going into the shell
+(def bearing-radius (+ 1.5 0.1)) ; 1.5 from 3mm diameter (+0.1 r for tolerance)
+(def dowel-depth-in-shell bearing-radius) ; -2.25 is just tangentially touching
+(def bearing-protrude (- (* 2 bearing-radius) dowel-depth-in-shell)) ; Radius of the baring minus how deep it's going into the shell
 (def trackball-width 34)
-(def trackball-width-plus-bearing (+ bearing-protrude trackball-width 0.5)) ; Add one just to give some wiggle
-(def holder-thickness 4.2)
-(def outer-width (+ (* 2 holder-thickness) trackball-width-plus-bearing))
+(def trackball-radius (/ trackball-width 2))
+(def trackball-bearing-wiggle -0.5)
+(def trackball-radius-plus-bearing (+ bearing-protrude trackball-radius trackball-bearing-wiggle)) ; Add one just to give some wiggle
+(def holder-thickness 4.2) ; thickness of the trackall casing
+(def outer-cup-radius (+ holder-thickness trackball-radius-plus-bearing))
 
 (def axel-angle 15)
-; (def dowell-width 3)
-; (def dowel-top-change 0)
-; (def dowel-top-height 1.5)
-; (def dowell-height 6) ; Dowel height is actually 6mm. But attempting to get it to "snap" in place
-; (def dowell (union (cylinder (- (/ dowell-width 2) dowel-top-change) (+ dowell-height dowel-top-height) :fn 50) (cylinder (/ dowell-width 2) dowell-height :fn 50)))
-; (def bearing (cylinder (/ 8.5 2) 3)) ; Bearing is actually 6mm x 2.5mm, model it as 8.5mm x 3 to give it room to spin
-; (def dowell-bearing (rotate (deg2rad 90) [1 0 0] (union dowell bearing)))
 (defn sphere_ [r sides]
   (let [args (merge {:r r}
                     (if *fa* {:fa *fa*})
@@ -905,15 +901,13 @@
                     (if *fs* {:fs *fs*}))]
     `(:sphere ~args)))
 
-(def dowell-bearing (sphere_ 1.5 35))
+(def sphere-bearing (sphere_ bearing-radius 35))
 (defn rotated_dowell [angle]
-  (rotate (deg2rad angle) [0, 0, 1] (rotate (deg2rad axel-angle) [0, 1, 0] (
-                                                                             translate [(+ (/ trackball-width-plus-bearing 2) dowel-depth-in-shell) 0 0] (union
-                                                                                                                                                          ; Add a cube on the side of the dowell so there's an insertion point when we diff with the shell
-                                                                                                                                                          ; (translate [(- (/ dowell-width 2)) 0 0] (cube (+ dowell-width 1) (- dowell-height dowel-top-change) dowell-width))
-                                                                                                                                                          dowell-bearing
-                                                                                                                                                          )
-                                                                                       )))
+  (rotate (deg2rad angle) [0, 0, 1] 
+          (rotate (deg2rad axel-angle) [0, 1, 0] 
+                  ( translate [trackball-radius-plus-bearing 0 0]
+                                sphere-bearing
+                              )))
   )
 
 (def dowells (union
@@ -926,20 +920,20 @@
 (def cup (
            difference
            (union
-            (sphere (/ outer-width 2)) ; Main cup sphere
-            (translate [0, 0, (/ vertical-hold 2)] (cylinder (/ outer-width 2) vertical-hold)) ; add a little extra to hold ball in
+            (sphere outer-cup-radius) ; Main cup sphere
+            (translate [0, 0, (/ vertical-hold 2)] (cylinder outer-cup-radius vertical-hold)) ; add a little extra to hold ball in
             )
-           (sphere (/ trackball-width-plus-bearing 2))
-           (translate [0, 0, (+ (/ outer-width 2) vertical-hold)] (cylinder (/ outer-width 2) outer-width)) ; cut out the upper part of the main cup spher
+           (sphere trackball-radius-plus-bearing)
+           (translate [0, 0, (+ outer-cup-radius vertical-hold)] (cylinder outer-cup-radius outer-cup-radius)) ; cut out the upper part of the main cup spher
            )
   )
 
 ; We know the ball will sit approx bearing-protrude over the sensor holder. Eliminate the bottom and make it square
 ; up to that point with trim
 (def trim (- (+ holder-thickness bearing-protrude) 0.5))
-(def bottom-trim-origin [0 0 (- (- (/ outer-width 2) (/ trim 2)))])
+(def bottom-trim-origin [0 0 (- (- outer-cup-radius (/ trim 2)))])
 (def bottom-trim ; trim the bottom off of the cup to get a lower profile
-  (translate bottom-trim-origin (cube outer-width outer-width trim))
+  (translate bottom-trim-origin (cube (* 2 outer-cup-radius) (* 2 outer-cup-radius) trim))
   )
 
 (def holder-negatives (union
@@ -948,7 +942,7 @@
                        )
   )
 (def cup-bottom
-  (translate [0 0 (- (- (/ outer-width 2) (/ trim 2)))] (cube outer-width outer-width trim))
+  (translate [0 0 (- (- outer-cup-radius (/ trim 2)))] (cube (* 2 outer-cup-radius) (* 2 outer-cup-radius) trim))
   )
 (def test-holder
   (
@@ -1039,7 +1033,7 @@
 
 ; This makes sure we can actually insert the trackball by leaving a column a little wider than it's width
 (def trackball-insertion-cyl (dowell-angle (translate [0 0 (- (/ trackball-width 2) (/ holder-thickness 2))]
-                             (cylinder (+ (/ trackball-width 2) 1) (+ (/ outer-width 2) 10))))
+                             (cylinder (+ (/ trackball-width 2) 1) (+ outer-cup-radius 10))))
   )
 
 (def trackball-raise (+ bearing-protrude 0.5))
@@ -1053,8 +1047,8 @@
 (def filler-half-circle (
                    ->>  (
                           difference
-                          (sphere (/ trackball-width-plus-bearing 2))
-                          (translate [0 0 (+ (/ outer-width 2) vertical-hold)] (cylinder (/ outer-width 2) outer-width)) ; cut out the upper part of the main cup spher
+                          (sphere trackball-radius-plus-bearing)
+                          (translate [0 0 (+ outer-cup-radius vertical-hold)] (cylinder outer-cup-radius (* 2 outer-cup-radius))) ; cut out the upper part of the main cup spher
                           )
                         (translate [0 0 trackball-raise])
                        filler-rotate
